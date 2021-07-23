62b095abad262591b8749698e38a9276a2e3d3eb
zce <w@zce.me>
2021-07-23 15:12:09 +0800

    chore: update template

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -1,7 +1,11 @@
+/**
+ * Vite configuaration file
+ * https://vitejs.dev/config/
+ */
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
 })
